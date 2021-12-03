e65360ab77fe5152917a79776aa9450f1b0836b2
TheMadMike <m.gibas2407@gmail.com>
2021-12-04 00:49:01 +0100

    Add include guard to cstring.h and include stddef.h

--- src/cstring.h
+++ src/cstring.h
@@ -1,3 +1,8 @@
+#ifndef CSTRING_H
+#define CSTRING_H
+
+#include <stddef.h>
+
 typedef struct CString
 {
     //TODO: fill
@@ -6,4 +11,5 @@ typedef struct CString
 CString* cstring_new(const char* str);
 
 //TODO: declare and define the rest of the specified functions
- 
+
+#endif //CSTRING_H
