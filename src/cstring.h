f142535c26b73cea7398200795bf6afa0c649ebf
TheMadMike <m.gibas2407@gmail.com>
2021-12-04 13:46:12 +0100

    Specify cstring_get() behaviour when index out of range

--- src/cstring.h
+++ src/cstring.h
@@ -14,6 +14,7 @@ CString* cstring_new(const char* str);
 // returns raw string copy
 char* cstring_raw(CString* string);
 
+// returns null character if index >= string->length
 char cstring_get(CString* string, size_t index);
 
 void cstring_set(CString* string, char c, size_t index);
